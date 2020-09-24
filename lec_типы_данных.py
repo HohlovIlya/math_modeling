26121a5ef76e2fa791463eb98878b1088b21445f
HohlovIlya <71829687+HohlovIlya@users.noreply.github.com>
2020-09-24 17:16:37 +0200

    Введение
    Классно (^-^)

---
+++ lec_типы_данных.py
@@ -0,0 +1,35 @@
+print(type(3))
+
+print(type(3.4))
+
+print(type('molodec'))
+
+a='MOLODEC'
+
+print(a)
+
+print(type([1,2,3]))
+
+a='good'
+ 
+b='bad'
+
+print(a+b) 
+
+a=[1,3,6] #создание списка
+
+print(a[0]) #вывод на экран нулевого элемента списка
+
+b=[8,10,11]
+
+c=a+b
+
+print(c)
+
+A=[]
+
+A.append(1)
+
+print(A)
+
+A.append(b)
