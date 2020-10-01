7c9848ccabcba3e29c06ca42d14fce38d312a5c6
HohlovIlya <71829687+HohlovIlya@users.noreply.github.com>
2020-10-01 17:45:36 +0200

    Add files via upload

---
+++ lab_3_pr_4.py
@@ -0,0 +1,3 @@
+a=int(input('введите число:_'))
+for a in range(0,10,1):
+    print(a,end=' ')
---
+++ lec_2_for.py
@@ -0,0 +1,13 @@
+for i in 1,3,4:
+    print(i**2,sep=' ')
+
+a=[1,5,7,10]
+for i in a:
+    print(i**3,end=' "Yes" ')
+    
+for i in range(0,10,1):
+    print(i,end=' ')
+    
+ 
+
+    
