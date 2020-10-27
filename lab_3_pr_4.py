3313234fc68c7423d94fe37a6b001c579cad69d3
HohlovIlya <71829687+HohlovIlya@users.noreply.github.com>
2020-10-27 17:14:53 +0200

    Сделал
    В принципе, понял

--- lab_3_pr_4.py
+++ lab_3_pr_4.py
@@ -1,3 +1,16 @@
-a=int(input('введите число:_'))
-for a in range(0,10,1):
-    print(a,end=' ')
+import numpy as np
+N=int(input('введите число:_'))
+M=int(input('введите число:_'))
+mxn=np.ndarray(shape=(N,M))
+for i in range(N):
+    for j in range(M):
+        if i>0 and j>0:
+            mxn[i][j]=np.sin(N*(i)+M*(j))
+            if mxn[i][j]<0:
+                mxn[i][j]=0
+        else:
+            mxn[i][j]=np.sin(N*(i+1)+M*(j+1))
+            if mxn[i][j]<0:
+                mxn[i][j]=0
+print(mxn)
+
